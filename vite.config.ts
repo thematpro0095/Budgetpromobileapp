7ac020c8288033538a5d9e14e5457906e6b1b81c
RENAN RICHARD DA SILVA SERGIO <renanrichardss20@gmail.com>
2025-10-29 20:05:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@
         'next-themes@0.4.6': 'next-themes',
         'lucide-react@0.487.0': 'lucide-react',
         'input-otp@1.4.2': 'input-otp',
-        'figma:asset/0314ca9ef2ad7a1d30a0d23fdfefdf6f06ebf23c.png': path.resolve(__dirname, './src/assets/0314ca9ef2ad7a1d30a0d23fdfefdf6f06ebf23c.png'),
+        '@logo': path.resolve(__dirname, './src/assets/0314ca9ef2ad7a1d30a0d23fdfefdf6f06ebf23c.png'),
         'embla-carousel-react@8.6.0': 'embla-carousel-react',
         'cmdk@1.1.1': 'cmdk',
         'class-variance-authority@0.7.1': 'class-variance-authority',

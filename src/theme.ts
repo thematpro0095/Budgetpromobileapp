32f23a64aa1d589a93d93b3ac7eed0c8c10e173a
RENAN RICHARD DA SILVA SERGIO <renanrichardss20@gmail.com>
2025-11-13 21:27:19 -0300

    Create theme.ts

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+export const themes = {
+  light: {
+    background: "bg-gray-50",
+    card: "bg-white",
+    text: "text-gray-900",
+  },
+  dark: {
+    background: "bg-gray-900",
+    card: "bg-gray-800",
+    text: "text-gray-100",
+  },
+};
